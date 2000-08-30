8bfc9271572fdb9688c8b562cc60d2ed69de95e1
keybuk <keybuk@36e7e03e-8437-11dd-8f23-d9c8888c2974>
2000-08-30 13:30:27 +0000

    Added new crypt program

---
+++ contrib/crypt.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#else /* HAVE_CONFIG_H */
+#include <unistd.h>
+#endif /* HAVE_CONFIG_H */
+
+#ifdef HAVE_CRYPT_H
+#include <crypt.h>
+#endif /* HAVE_CRYPT_H */
+
+static void _saltchar(char *);
+
+int main() {
+  char pass[80], salt[3], *ret;
+  
+  printf("Enter a password to encrypt: ");
+  ret = fgets(pass, sizeof(pass), stdin);
+  if (ret) {
+    char *ptr, *enc;
+
+    ptr = pass + strlen(pass);
+    while ((ptr >= ret) && (*ptr <= 32)) *(ptr--) = 0;
+
+    srand(time(0));
+    _saltchar(&(salt[0]));
+    _saltchar(&(salt[1]));
+    salt[2] = 0;
+
+    enc = crypt(pass, salt);
+    printf("\nPassword encrypted is: %s\n", enc);
+  } else {
+    printf("\nNo password received.\n");
+  }
+}
+
+static void _saltchar(char *c) {
+  static char *chars = "abcdefghijklmnopqrstuvwxyz"
+                       "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                       "01234567890";
+  int ran;
+
+  ran = (int)((double)(strlen(chars) - 1) * (rand() / (RAND_MAX + 1.0)));
+  *c = chars[ran];
+}
