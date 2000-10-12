88d449ef88f75d47f34b436e4fea5f52613b4d3e
keybuk <keybuk@36e7e03e-8437-11dd-8f23-d9c8888c2974>
2000-10-12 16:00:39 +0000

    Almost total rewrite of the DNS code.  Now you have to request a DNS and provide a function to run once the DNS lookup is complete.  The code will then fork() off to do the lookup and continue with normal execution of the program.  When the client exits, the result is on a pipe, and once read execution is continued from the function you give it.
    (ie non-blocking DNS :o)

--- trunk/src/dns.h
+++ trunk/src/dns.h
@@ -4,7 +4,7 @@
  *
  * dns.h
  * --
- * @(#) $Id: dns.h,v 1.1 2000/05/13 02:13:42 keybuk Exp $
+ * @(#) $Id: dns.h,v 1.2 2000/10/12 16:00:39 keybuk Exp $
  *
  * This file is distributed according to the GNU General Public
  * License.  For full details, read the top of 'main.c' or the
@@ -19,12 +19,25 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include "irc_net.h"
+
+
 /* functions */
-extern int dns_addrfromhost(const char *, struct in_addr *, char **);
-extern char *dns_hostfromaddr(struct in_addr);
+extern int dns_endrequest(pid_t, int);
+extern int dns_delall(struct ircproxy *);
+extern void dns_flush(void);
+extern int dns_addrfromhost(struct ircproxy *, void *, const char *,
+                         void (*)(struct ircproxy *, void *,
+                                  struct in_addr *, const char *));
+extern int dns_hostfromaddr(struct ircproxy *, void *, struct in_addr,
+                         void (*)(struct ircproxy *, void *,
+                                  struct in_addr *, const char *));
+extern int dns_filladdr(struct ircproxy *, const char *, const char *, int,
+                        struct sockaddr_in *,
+                        void (*)(struct ircproxy *, void *,
+                                 struct in_addr *, const char *),
+                        void *);
 extern short dns_portfromserv(const char *);
 extern char *dns_servfromport(short);
-extern int dns_filladdr(const char *, const char *, int, struct sockaddr_in *,
-                        char **);
 
 #endif /* __DIRCPROXY_DNS_H */
