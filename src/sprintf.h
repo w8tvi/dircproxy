c6a9e1dd2cf44646256425709123d26a0581af99
keybuk <keybuk@36e7e03e-8437-11dd-8f23-d9c8888c2974>
2000-10-13 17:02:26 +0000

    Yay, thanks to l33t Demon staff, they dug out a new C99 syntax feature that GCC supports that lets you do variable argument macros.  Because this is debug mode I'm happy to put this C99 stuff in :)

--- src/sprintf.h
+++ src/sprintf.h
@@ -4,7 +4,7 @@
  *
  * sprintf.h
  * --
- * @(#) $Id: sprintf.h,v 1.3 2000/10/13 12:53:10 keybuk Exp $
+ * @(#) $Id: sprintf.h,v 1.4 2000/10/13 17:02:26 keybuk Exp $
  *
  * This file is distributed according to the GNU General Public
  * License.  For full details, read the top of 'main.c' or the
@@ -19,18 +19,10 @@
 
 /* functions */
 #ifdef DEBUG_MEMORY
-/* Sorry about this, its pretty disgusting.  Because x_sprintf() is a ...
-   function we can't actually just make a #define wrapper for it tagging
-   a couple of extra parameters in it.  Instead we have to call a function
-   called xx_set to set the file and line number parameters, then have this
-   return a pointer to the real x_sprintf() function - cast it - then use
-   the following brackets to actually call it. */
-#define x_sprintf ((char *(*)(const char *, ...))xx_set(__FILE__, __LINE__))
-extern void *xx_set(char *, int);
-
-/* These are just wrappers around their usual function */
+#define x_sprintf(ARGS...) xx_sprintf(__FILE__, __LINE__, ARGS)
 #define x_vsprintf(FMT, LIST) xx_vsprintf(__FILE__, __LINE__, FMT, LIST)
 #define x_strdup(STR) xx_strdup(__FILE__, __LINE__, STR)
+extern char *xx_sprintf(char *, int, const char *, ...);
 extern char *xx_vsprintf(char *, int, const char *, va_list);
 extern char *xx_strdup(char *, int, const char *);
 #else /* DEBUG_MEMORY */
