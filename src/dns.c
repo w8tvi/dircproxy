8918cd301ae8190d80fa39e412afb682085d826d
keybuk <keybuk@36e7e03e-8437-11dd-8f23-d9c8888c2974>
2000-11-10 15:08:02 +0000

    Added an extra pipe to avoid race conditions, also actually check the wait() status properly

--- src/dns.c
+++ src/dns.c
@@ -11,7 +11,7 @@
  * of a DNS request is notified by the child death signal, so it will
  * interrupt the main loop to continue where you left off.
  * --
- * @(#) $Id: dns.c,v 1.10 2000/11/06 16:57:43 keybuk Exp $
+ * @(#) $Id: dns.c,v 1.11 2000/11/10 15:08:02 keybuk Exp $
  *
  * This file is distributed according to the GNU General Public
  * License.  For full details, read the top of 'main.c' or the
@@ -23,6 +23,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -139,6 +140,8 @@ static int _dns_startrequest(void *boundto,
     dnschildren = child->next;
     close(p[1]);
     close(p[0]);
+    close(wp[1]);
+    close(wp[0]);
     free(child);
     function(boundto, data, 0, 0);
     return -1;
@@ -217,28 +220,34 @@ int dns_endrequest(pid_t pid, int status) {
   addr = 0;
   
   /* Read from pipe if child returned normally */
-  if (!status) {
-    len = read(child->pipe, (void *)&result, sizeof(struct dnsresult));
-    if (len != sizeof(struct dnsresult)) {
-      syscall_fail("read", 0, 0);
-    } else if (result.success) {
-      debug("%d: Got result", pid);
+  if (WIFEXITED(status)) {
+    if (!WEXITSTATUS(status)) {
+      len = read(child->pipe, (void *)&result, sizeof(struct dnsresult));
+      if (len != sizeof(struct dnsresult)) {
+        syscall_fail("read", 0, 0);
+      } else if (result.success) {
+        debug("%d: Got result", pid);
 
-      addr = &(result.addr);
-      if (!strlen(result.name)) {
-        char *ip;
+        addr = &(result.addr);
+        if (!strlen(result.name)) {
+          char *ip;
 
-        /* No name received, so copy IP address into it */
-        ip = inet_ntoa(result.addr);
-        strncpy(result.name, ip, 255);
-        result.name[255] = 0;
+          /* No name received, so copy IP address into it */
+          ip = inet_ntoa(result.addr);
+          strncpy(result.name, ip, 255);
+          result.name[255] = 0;
 
-        debug("%d: Changed name to '%s'", pid, result.name);
+          debug("%d: Changed name to '%s'", pid, result.name);
+        }
+        name = result.name;
       }
-      name = result.name;
+    } else {
+      debug("%d: DNS lookup returned %d", pid, WEXITSTATUS(status));
     }
+  } else if (WIFSIGNALED(status)) {
+    debug("%d: DNS lookup terminated with signal %d", pid, WTERMSIG(status));
   } else {
-    debug("%d: DNS lookup returned %d", pid, status);
+    debug("%d: DNS lookyp terminated abnormally", pid);
   }
 
   /* Call the function */
