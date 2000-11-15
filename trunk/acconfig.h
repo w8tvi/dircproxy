7780b331a12e4f300949cff43d956ef032119db3
keybuk <keybuk@36e7e03e-8437-11dd-8f23-d9c8888c2974>
2000-11-15 15:56:41 +0000

    Added new typedefs to stop autoconf moaning

--- trunk/acconfig.h
+++ trunk/acconfig.h
@@ -9,3 +9,9 @@
 
 /* Turn on expensive (but cool) memory debugging */
 #undef DEBUG_MEMORY
+
+/* Define to uint_32_t if not in <sys/types.h> */
+#undef u_int32_t
+
+/* Define if not in <sys/types.h> */
+#undef uint32_t
