19d669f310d8d07e639c7ed418697a9a697aa5f6
keybuk <keybuk@36e7e03e-8437-11dd-8f23-d9c8888c2974>
2000-12-07 17:17:06 +0000

    Large rework of DCC Send proxying.  We now store everything we get from the sender in an internal buffer until we can send it out to the sendee. This means the sender can disconnect part way through without the whole lot going *splegh* all over the place.

--- src/dcc_send.c
+++ src/dcc_send.c
@@ -5,7 +5,7 @@
  * dcc_send.c
  *  - DCC send protocol
  * --
- * @(#) $Id: dcc_send.c,v 1.5 2000/11/20 15:25:54 keybuk Exp $
+ * @(#) $Id: dcc_send.c,v 1.6 2000/12/07 17:17:06 keybuk Exp $
  *
  * This file is distributed according to the GNU General Public
  * License.  For full details, read the top of 'main.c' or the
@@ -33,6 +33,7 @@
 /* forward declarations */
 static void _dccsend_data(struct dccproxy *, int);
 static void _dccsend_error(struct dccproxy *, int, int);
+static int _dccsend_sendpacket(struct dccproxy *);
 
 /* Called when we've connected to the sender */
 void dccsend_connected(struct dccproxy *p, int sock) {
@@ -62,6 +63,7 @@ void dccsend_connectfailed(struct dccproxy *p, int sock, int bad) {
   debug("DCC Connection failed");
   p->sender_status &= ~(DCC_SENDER_CREATED);
   net_close(p->sender_sock);
+  p->sender_sock = -1;
   p->dead = 1;
 }
 
@@ -70,27 +72,48 @@ void dccsend_accepted(struct dccproxy *p) {
   net_hook(p->sendee_sock, SOCK_NORMAL, (void *)p,
            ACTIVITY_FUNCTION(_dccsend_data),
            ERROR_FUNCTION(_dccsend_error));
+
+  /* If we've already got data, we better some */
+  if (p->bufsz)
+    _dccsend_sendpacket(p);
 }
 
 /* Called when we get data over a DCC link */
 static void _dccsend_data(struct dccproxy *p, int sock) {
   if (sock == p->sender_sock) {
-    if ((p->sendee_status != DCC_SENDEE_ACTIVE)
-        && !(p->type & DCC_SEND_CAPTURE))
-      return;
+    int buflen, nr;
+
+    /* Read the data into the buffer */
+    buflen = net_read(p->sender_sock, 0, 0);
+    p->buf = (char *)realloc(p->buf, p->bufsz + buflen);
+    nr = net_read(p->sender_sock, (void *)(p->buf + p->bufsz), buflen);
+
+    /* Check we read some */
+    if (nr > 0) {
+      uint32_t na;
+
+      p->bufsz += nr;
+      p->bytes_rcvd += nr;
+
+      debug("Accepted %d bytes from sender (tot: %lu  buf: %lu)",
+            nr, p->bytes_rcvd, p->bufsz);
+
+      /* Acknowledge them */
+      na = htonl(p->bytes_rcvd);
+      net_queue(p->sender_sock, (void *)&na, sizeof(uint32_t));
+    } else {
+      p->buf = (char *)realloc(p->buf, p->bufsz);
+    }
 
   } else if (sock == p->sendee_sock) {
     uint32_t ack;
     int len;
 
-    if (p->sender_status != DCC_SENDER_ACTIVE)
-      return;
+    /* We should only ever get ack's back */
+    len = net_read(p->sendee_sock, (void *)&ack, sizeof(uint32_t));
+    if (len == sizeof(uint32_t))
+      p->bytes_ackd = ntohl(ack);
 
-    if (p->type & DCC_SEND_SIMPLE) {
-      len = net_read(p->sendee_sock, (void *)&ack, sizeof(uint32_t));
-      if (len == sizeof(uint32_t))
-        p->bytes_ackd = ntohl(ack);
-    }
   } else {
     error("Unexpected socket %d in dccsend_data, expected %d or %d", sock,
           p->sender_sock, p->sendee_sock);
@@ -98,42 +121,34 @@ static void _dccsend_data(struct dccproxy *p, int sock) {
     return;
   }
 
-  if ((p->type & DCC_SEND_FAST) || (p->type & DCC_SEND_CAPTURE) || 
-      (p->bytes_ackd >= p->bytes_sent)) {
-    int buflen;
+  debug("rcvd: %lu  sent: %lu  ackd: %lu  (buf: %lu)",
+        p->bytes_rcvd, p->bytes_sent, p->bytes_ackd, p->bufsz);
 
-    buflen = net_read(p->sender_sock, 0, 0);
-    if (buflen) {
-      char *buf;
-      int nr;
+  /* Receiving data is as good as trigger as any to check whether we can send
+     more. */
+  if (p->bufsz && ((p->type & DCC_SEND_FAST) || (p->type & DCC_SEND_CAPTURE) || 
+                   (p->bytes_ackd >= p->bytes_sent))) {
+    /* Capturing?  Just eat the buffer right here, right now */
+    if (p->type & DCC_SEND_CAPTURE) {
+      /* Write it to the file */
+      fwrite((void *)p->buf, 1, p->bufsz, p->cap_file);
 
-      buf = (char *)malloc(buflen);
-      nr = net_read(p->sender_sock, (void *)buf, buflen);
-      if (nr > 0) {
-        uint32_t na;
+      /* Sent the whole thing */
+      p->bytes_sent += p->bufsz;
+      p->bufsz = 0;
+      free(p->buf);
+      p->buf = 0;
 
-        p->bytes_sent += nr;
-
-        if (p->type & DCC_SEND_CAPTURE) {
-          /* Write it to the file */
-          fwrite((void *)buf, 1, nr, p->cap_file);
-
-          /* Check we haven't exceeded the maximum size */
-          if (p->bytes_max && (p->bytes_sent >= p->bytes_max)) {
-            /* We have, kill it.  It'll automatically get unlinked */
-            debug("Too big for my boots!");
-            p->dead = 1;
-          }
-        } else {
-          /* Send it to the sendee */
-          net_queue(p->sendee_sock, (void *)buf, nr);
-        }
-
-        /* Report how many bytes we've ackd so far */
-        na = htonl(p->bytes_sent);
-        net_queue(p->sender_sock, (void *)&na, sizeof(uint32_t));
+      /* Check we haven't exceeded the maximum size */
+      if (p->bytes_max && (p->bytes_sent >= p->bytes_max)) {
+        /* We have, kill it.  It'll automatically get unlinked */
+        debug("Too big for my boots!");
+        p->dead = 1;
       }
-      free(buf);
+
+    } else if (p->sendee_status == DCC_SENDEE_ACTIVE) {
+      /* Send packet to the client */
+      _dccsend_sendpacket(p);
     }
   }
 }
@@ -146,10 +161,21 @@ static void _dccsend_error(struct dccproxy *p, int sock, int bad) {
     who = "Sender";
     p->sender_status &= ~(DCC_SENDER_CREATED);
     net_close(p->sender_sock);
+    p->sender_sock = -1;
+
+    /* Not necessarily bad, just means the client has gone */
+    if (p->bufsz && !(p->type & DCC_SEND_CAPTURE)) {
+      debug("%lu bytes remaining", p->bufsz);
+      p->sender_status = DCC_SENDER_GONE;
+    } else {
+      p->dead = 1;
+    }
   } else if (sock == p->sendee_sock) {
     who = "Sendee";
     p->sendee_status &= ~(DCC_SENDEE_CREATED);
     net_close(p->sendee_sock);
+    p->sendee_sock = -1;
+    p->dead = 1;
   } else {
     error("Unexpected socket %d in dccsend_error, expected %d or %d", sock,
           p->sender_sock, p->sendee_sock);
@@ -171,6 +197,40 @@ static void _dccsend_error(struct dccproxy *p, int sock, int bad) {
       p->cap_file = 0;
     }
   }
+}
 
-  p->dead = 1;
+/* Send a packet of buffered data to the client */
+static int _dccsend_sendpacket(struct dccproxy *p) {
+  unsigned long nr;
+
+  /* If we're doing simple sends, we limit the amount we send, if doing fast
+     just shove the whole lot to them */
+  if (p->type & DCC_SEND_FAST) {
+    nr = p->bufsz;
+  } else {
+    nr = (p->bufsz > DCC_BLOCK_SIZE ? DCC_BLOCK_SIZE : p->bufsz);
+  }
+
+  /* Send it to the sendee */
+  if (nr) {
+    net_queue(p->sendee_sock, (void *)p->buf, nr);
+    debug("Sending %lu bytes to client", nr);
+    
+    /* Adjust or free the buffer */
+    p->bytes_sent += nr;
+    p->bufsz -= nr;
+    if (p->bufsz) {
+      memmove(p->buf, p->buf + nr, p->bufsz);
+      p->buf = (char *)realloc(p->buf, p->bufsz);
+    } else {
+      free(p->buf);
+      p->buf = 0;
+    }
+  }
+
+  /* Out of buffer and the sender has gone */
+  if (!p->bufsz && (p->sender_status == DCC_SENDER_GONE))
+    p->dead = 1;
+
+  return nr;
 }
