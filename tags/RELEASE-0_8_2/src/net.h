9168b780f75c71d7de752b42c0003ee6f72ae1d7
unknown <unknown@36e7e03e-8437-11dd-8f23-d9c8888c2974>
2004-05-15 16:10:18 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE-0_8_2'.

---
+++ tags/RELEASE-0_8_2/src/net.h
@@ -0,0 +1,42 @@
+/* dircproxy
+ * Copyright (C) 2000 Scott James Remnant <scott@netsplit.com>.
+ * All Rights Reserved.
+ *
+ * net.h.h
+ * --
+ * @(#) $Id: net.h,v 1.4 2000/11/01 15:02:00 keybuk Exp $
+ *
+ * This file is distributed according to the GNU General Public
+ * License.  For full details, read the top of 'main.c' or the
+ * file called COPYING that was distributed with this code.
+ */
+
+#ifndef __DIRCPROXY_NET_H
+#define __DIRCPROXY_NET_H
+
+/* Socket types */
+#define SOCK_NORMAL     0x00
+#define SOCK_CONNECTING 0x01
+#define SOCK_LISTENING  0x02
+
+/* handy defines */
+#define ACTIVITY_FUNCTION(_FUNC) ((void (*)(void *, int)) (_FUNC))
+#define ERROR_FUNCTION(_FUNC) ((void (*)(void *, int, int)) (_FUNC))
+
+/* functions */
+extern int net_socket(void);
+extern void net_create(int *);
+extern int net_close(int);
+extern int net_closeall(void);
+extern int net_flush(void);
+extern int net_hook(int, int, void *,
+                    void(*)(void *, int), void(*)(void *, int, int));
+extern int net_throttle(int, long, long);
+extern int net_send(int, const char *, ...);
+extern int net_sendurgent(int, const char *, ...);
+extern int net_queue(int, void *, int);
+extern int net_gets(int, char **, const char *);
+extern int net_read(int, void *, int);
+extern int net_poll(void);
+
+#endif /* __DIRCPROXY_NET_H */
